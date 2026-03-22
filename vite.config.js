0ba197ef35e38c3578bcde26ef4bf87db60e2b31
Betsy”git config --global user.email “betsy”.z.ellison@gmail.com  git config --global user.name Betsy” <betsy.z.ellison@gmail.com>
2026-03-22 13:42:29 -0500

    initial commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+
+export default {
+  base: '/my-calendar/',   // must match your GitHub repo name exactly
+  plugins: [react()],
+}
